32a29812401575b2d34e4900aa7c3e15e2018e97
lesquive <luis.jose.esquivel@gmail.com>
2023-01-01 12:24:50 -0600

    added practice files

---
+++ lección 1/1.5-variablesPractica.js
@@ -0,0 +1,42 @@
+// @CISR.
+// preguntas de practica variables
+
+// ¿De qué tipo es el siguiente valor?
+
+"hello";
+
+// ¿De qué tipo es el siguiente valor?
+
+true;
+
+// ¿De qué tipo es el siguiente valor?
+
+[1, 2, 3];
+
+// ¿De qué tipo es el siguiente valor?
+
+{
+  name: "John";
+}
+
+// ¿De qué tipo es el siguiente valor?
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+// ¿De qué tipo es el siguiente valor?
+
+null;
+
+// ¿De qué tipo es el siguiente valor?
+
+undefined;
+
+// ¿De qué tipo es el siguiente valor?
+
+1;
+
+// ¿De qué tipo es el siguiente valor?
+
+NaN;
---
+++ lección 2/2.4-operacionesNumericasPractica.js
@@ -0,0 +1,42 @@
+// @CISR.
+// preguntas de practica aritmetica
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
+3 + 5 * 8;
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
+(3 + 5) * 8;
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
+23 % 8;
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
+2 ** 3;
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
+-(5 + 7);
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
++(-(2 + 5) + 7);
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
+5 / "2";
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
+"3" * "8";
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
+"3" + "8";
+
+// ¿Cuál es el resultado de la siguiente expresión?
+
+"3" - "8";
