8dd2f75dd3d5ab25d5bb9ac895fa255715730584
lesquive <luis.jose.esquivel@gmail.com>
2023-01-09 18:15:53 -0600

    added strings practice

---
+++ lección 3/3.3-stringsPractica.js
@@ -0,0 +1,45 @@
+// @CISR.
+// preguntas de practica manipulación y comprensión de Strings
+
+// 1) ¿Cuál es la longitud del string "Hello, World!"?
+
+let message = "Hello, World!";
+
+// 2)  ¿Cómo accede al primer carácter de un string en JS?
+
+message = "Hello, World!";
+
+// 3) ¿Cómo accede al último carácter de un string en JS?
+
+message = "Hello, World!";
+
+// 4) ¿Cómo se convierte un string para tener todas las letras mayúsculas usando JS?? (investigar el metodo JS "toUpperCase")
+
+message = "Hello, World!";
+
+// 5) ¿Cómo se convierte un string para tener todas las letras minúsculas en JS? (investigar el metodo JS "toLowerCase")
+
+message = "Hello, World!";
+
+// 6) ¿Cómo se extrae una parte de un string en JS? (investigar el metodo JS "substring")
+
+message = "Hello, World!";
+
+// 7) ¿Cómo concatenas (combinas) dos strings en JS?
+
+let mensaje = "Amo aprender";
+let mensaje1 = " nuevas cosas.";
+
+// 8) ¿Cómo se verifica si un string incluye un determinado carácter o caracteres en JS? (investigar el metodo JS "includes")
+
+message = "Hello, World!";
+
+// 9)  ¿Cómo accede al tercer carácter de un string en JS?
+
+message = "Hello, World!";
+
+//Puntos extra:
+// 10) ¿Cómo se verifica si una cadena está en mayúsculas en JS?
+
+let min = "hello, world!";
+let may = "HELLO, WORLD!";
