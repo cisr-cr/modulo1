89a89bb42ab0b272e4b05933f5d1a93276df33ce
Luis <51721338+lesquive@users.noreply.github.com>
2023-01-10 20:39:49 -0600

    Created Arrays practica file

---
+++ lección 4/arraysPractica.js
@@ -0,0 +1,52 @@
+// @CISR.
+// preguntas de practica manipulación y comprensión de Strings
+
+// 1) ¿Cuál es el resultado del siguiente código?
+
+let arr = [1, 2, 3, 4];
+console.log(arr.pop());
+
+// 2) ¿Cuál es el resultado del siguiente código?
+
+let arr = [1, 2, 3, 4];
+console.log(arr.shift());
+
+// 3) ¿Cómo puede agregar el numero '5' al final de la siguiente matriz?
+
+let arr = [1, 2, 3, 4];
+
+// 4) ¿Cómo puede agregar el elemento '0' al comienzo de la siguiente matriz?
+
+let arr = [1, 2, 3, 4];
+
+// 5) ¿Cuál es el resultado del siguiente código?
+
+let arr = [1, 2, 3, 4];
+console.log(arr.includes(3));
+
+// 6) ¿Cómo puede encontrar el índice del elemento '3' en la siguiente matriz? Investigar "indexOf()".
+
+let arr = [1, 2, 3, 4];
+
+// 7) ¿Cuál es el resultado del siguiente código?
+
+let arr = [1, 2, 3, 4];
+console.log(arr.join("-"));
+
+// 8) ¿Cómo puede dividir los elementos del índice 1 al 2 en la siguiente matriz?
+
+let arr = [1, 2, 3, 4];
+
+// 9) ¿Cómo se puede invertir el orden de los elementos en la siguiente matriz? Investigar "reverse()".
+
+let arr = [1, 2, 3, 4];
+
+// 10) ¿Cómo puede ordenar los elementos en la siguiente matriz en orden ascendente? Investigar "sort()".
+
+let arr = [4, 2, 3, 1];
+
+// 11) ¿Cómo se puede encontrar la longitud de la siguiente matriz:
+
+let arr = [1, 2, 3, 4];
+
+
