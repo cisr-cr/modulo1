7db8f62c36b04da4eb05cde67263a6e6db19b2ad
Luis <51721338+lesquive@users.noreply.github.com>
2023-01-15 11:51:31 -0600

    Created Functions Practice Questions.

---
+++ lección 5/5.1-functionsPractica.js
@@ -0,0 +1,43 @@
+// @CISR.
+// preguntas de practica para la comprensión de Functions en JS. 
+
+// 1) Escribe una función que tome un solo argumento y devuelva su cuadrado.
+
+// 2) Escribe una función que tome dos argumentos y devuelva la suma de los dos números.
+
+// 3) Escriba una función que tome un string como argumento y devuelva la longitud del string.
+
+// 4) Escriba una función que tome una matriz como argumento y devuelva la suma de todos los números de la matriz.
+
+function sumaArr(arr) {}
+console.log(sumaArr([1, 2, 3, 4, 5])); // resultado: 15
+
+// 5) Escribe una función que tome un número como argumento y devuelva si el número es par o impar.
+
+function parOImpar(num) {}
+console.log(parOImpar(5)); // resultado: "impar"
+
+// 6) Escriba una función que tome dos strings como argumentos y devuelva la concatenación de las dos strings.
+
+function concatenate(str1, str2) {}
+console.log(concatenate("Hola ", "Mundo!")); // Output: "Hola Mundo!"
+
+// 7) Escriba una función que tome un string como argumento y devuelva la primera letra del string.
+
+function getPrimeraLetra(str) {}
+console.log(getPrimeraLetra("Costa Rica!")); // Output: "C"
+
+// 8) Escribe una función que tome 3 argumentos y devuelva la multiplicación de los tres números.
+
+// 9) Escriba una función que tome dos argumentos y también declare una variable local x =10. Dentro de la función, devuelva su suma de x con los dos argumentos.
+
+function suma(a, b) {}
+console.log(suma(3, 4)); // resultado: 17
+
+// 10) Convierta la siguiente función a notación de flecha:
+
+function multiPorDos(x) {
+  return x * 2;
+}
+
+
