c3141e93cdeff9fc84d103a1c84b1b03a3e1ac2c
lesquive <luis.jose.esquivel@gmail.com>
2023-01-23 17:44:34 -0600

    added bool practice questions

---
+++ lección 6/6.1-boolsPractica.js
@@ -0,0 +1,24 @@
+// @CISR.
+// preguntas de practica para la comprensión de booleanos en JS.
+
+// 1) ¿Cuál es el resultado de (!true)?
+
+// 2) ¿Cuál es el resultado de (!!true)?
+
+// 3) ¿Cuál es la diferencia entre == y ===?
+
+// 4) ¿Cómo puede usar el operador OR lógico (||) para proporcionar un valor predeterminado para una variable?
+
+// 5) ¿Cómo se verifica si una variable es mayor que 15?
+
+// 6) ¿Cómo se comprueba si una variable es menor o igual a 33?
+
+// 7) ¿Cómo se comprueba si dos variables son iguales?
+
+// 8) ¿Cómo usa el operador NOT para verificar si una variable no es igual a un valor específico en JavaScript?
+
+// 9) ¿Cómo se usa el operador OR para verificar si una variable es "valor1" o "valor2"?
+
+// 10) ¿Cómo se usa el operador AND para comprobar si una variable es mayor que 10 y menor que 20?
+
+// 11) ¿Cómo se verifica si una variable es mayor o igual a 50 pero menor a 100?
