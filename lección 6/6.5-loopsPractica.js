0a9927c44bfd52b493a38b17bb33a59c66798545
lesquive <luis.jose.esquivel@gmail.com>
2023-01-29 21:07:33 -0600

    added loops practica

---
+++ lección 6/6.5-loopsPractica.js
@@ -0,0 +1,34 @@
+// @CISR.
+// preguntas de practica para la comprensión de loops en JS.
+
+// 1) Escriba un loop for para registrar los números del 1 al 10 en la consola.
+
+// 2) Escriba un ciclo for para registrar solo los números pares del 1 al 10 en la consola.
+
+// 3) Escriba un while loop para registrar los números del 1 al 10 en la consola.
+
+// 4) Escriba un ciclo while para registrar solo los números pares del 1 al 10 en la consola.
+
+// 5) Escriba un ciclo for para registrar los elementos de una matriz en la consola.
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// 6) Escriba un ciclo for para sumar todos los números en una matriz.
+
+let numeros1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// 7) Escribe un loop for para encontrar el número más grande en una matriz.
+
+let numeros2 = [10, 15, 2, 7, 9, 22, 1];
+
+// 8) Escriba un loop for...in para registrar las propiedades de un objeto en la consola.
+
+let estudiante = {
+  name: "Jose",
+  age: 17,
+  job: "Developer",
+};
+
+// 9) Escriba un loop for...of para registrar los valores de una matriz en la consola.
+
+let valores = ["a", "b", "c", "d", "e"];
