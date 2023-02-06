0d9b466142bcf948f4fb51d389b4906254bc9937
lesquive <luis.jose.esquivel@gmail.com>
2023-02-05 20:17:08 -0600

    added switch practice

---
+++ lección 7/7.1-switchPractica.js
@@ -0,0 +1,21 @@
+// @CISR.
+// preguntas de practica manipulación y comprensión de Switch Case.
+
+// 1) Escriba una declaración de switch que pruebe val y establezca la respuesta para las siguientes condiciones:
+
+// 1 - alfa
+// 2 - beta
+// 3 - gama
+// 4 - delta
+
+// 2)Escriba una declaración de switch para establecer la respuesta para las siguientes condiciones:
+
+// a-manzana
+// b-pájaro
+// c-gato
+// default - otros
+
+// 3) Escriba una declaración de switch para establecer la respuesta para los siguientes rangos:
+// 1-3 - Bajo
+// 4-6 - Medio
+// 7-9 - Alto
